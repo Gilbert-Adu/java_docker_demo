808d609a78a4325ed3e3b5fed955a6080ff32241
Gilbert Adu <gea33@cornell.edu>
2024-08-29 10:21:32 -0400

    frontend completed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
+import {useState, useEffect} from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
+  const [javaData, setJavaData] = useState('');
+
+  useEffect(() => {
+    const getString = async() => {
+      try {
+        const response = await axios.get('http://localhost:8080/api/hello');
+        setJavaData(response.data)
+  
+      }catch(err) {
+        console.log(err.message)
+
+      }
+  
+
+    }
+    getString();
+
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Java test demo app
+      Our data: {javaData}
     </div>
   );
 }
