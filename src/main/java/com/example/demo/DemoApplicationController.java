7bd3703787652b08677da381306c6754b3b99c0f
Gilbert Adu <gea33@cornell.edu>
2024-08-29 10:21:48 -0400

    backend completed

--- src/main/java/com/example/demo/DemoApplicationController.java
+++ src/main/java/com/example/demo/DemoApplicationController.java
@@ -2,6 +2,7 @@ package com.example.demo;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 
 
@@ -9,7 +10,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @RequestMapping("/api")
 public class DemoApplicationController {
 
-
+    @CrossOrigin(origins = "http://localhost:5000")
     @GetMapping("/hello")
     public String sayHello() {
         return "Hello from the Java backend";
